5196c3cf9d5dc8ed2f9509c3c5f0e7d62e5c6155
Jaemin.Park <once29@naver.com>
2023-12-04 03:08:19 +0900

    Add GoalCheeringRepository

---
+++ raisedragon-core/src/main/kotlin/com/whatever/raisedragon/domain/goalcheering/GoalCheeringRepository.kt
@@ -0,0 +1,5 @@
+package com.whatever.raisedragon.domain.goalcheering
+
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface GoalCheeringRepository : JpaRepository<GoalCheeringEntity, Long>
