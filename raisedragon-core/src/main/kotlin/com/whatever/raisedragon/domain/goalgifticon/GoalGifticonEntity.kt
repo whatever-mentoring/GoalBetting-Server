1907931ae325b99b5d5acb3e1b414be27045f4d5
Jaemin.Park <once29@naver.com>
2023-12-04 03:01:10 +0900

    Add GoalGifticonEntity with DTO

---
+++ raisedragon-core/src/main/kotlin/com/whatever/raisedragon/domain/goalgifticon/GoalGifticonEntity.kt
@@ -0,0 +1,19 @@
+package com.whatever.raisedragon.domain.goalgifticon
+
+import com.whatever.raisedragon.domain.BaseEntity
+import com.whatever.raisedragon.domain.gifticon.GifticonEntity
+import com.whatever.raisedragon.domain.goal.GoalEntity
+import jakarta.persistence.*
+
+@Table(name = "goal_gifticon")
+@Entity
+class GoalGifticonEntity(
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "goal_id")
+    val goalEntity: GoalEntity,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "gifticon_id")
+    val gifticon: GifticonEntity,
+) : BaseEntity()
