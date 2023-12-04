5e859fce595952f20308103421ceaa93ee84afee
Jaemin.Park <once29@naver.com>
2023-12-04 17:53:32 +0900

    Fix broken repository

--- raisedragon-core/src/main/kotlin/com/whatever/raisedragon/domain/gifticon/GifticonRepository.kt
+++ raisedragon-core/src/main/kotlin/com/whatever/raisedragon/domain/gifticon/GifticonRepository.kt
@@ -4,4 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface GifticonRepository : JpaRepository<Gifticon, Long>
+interface GifticonRepository : JpaRepository<GifticonEntity, Long>
