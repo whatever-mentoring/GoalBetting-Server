f7bc36b743ec5d63a75e8f7afddf1cb5fb0558f8
Diger <kitten.diger@gmail.com>
2023-12-06 15:14:07 +0900

    Modify DTO ResponseForm

--- raisedragon-api/src/main/kotlin/com/whatever/raisedragon/applicationservice/GoalProofApplicationService.kt
+++ raisedragon-api/src/main/kotlin/com/whatever/raisedragon/applicationservice/GoalProofApplicationService.kt
@@ -1,18 +1,11 @@
 package com.whatever.raisedragon.applicationservice
 
-import com.whatever.raisedragon.applicationservice.dto.GoalProofResponse
-import com.whatever.raisedragon.applicationservice.dto.UserResponse
 import com.whatever.raisedragon.controller.goalproof.GoalProofCreateUpdateResponse
 import com.whatever.raisedragon.controller.goalproof.GoalProofRetrieveResponse
-import com.whatever.raisedragon.domain.goal.*
 import com.whatever.raisedragon.domain.goalproof.Document
-import com.whatever.raisedragon.domain.goalproof.GoalProof
 import com.whatever.raisedragon.domain.goalproof.GoalProofService
-import com.whatever.raisedragon.domain.user.Nickname
-import com.whatever.raisedragon.domain.user.User
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import java.time.LocalDateTime
 
 @Service
 @Transactional
@@ -22,63 +15,21 @@ class GoalProofApplicationService(
 
     fun create(): GoalProofCreateUpdateResponse {
         return GoalProofCreateUpdateResponse(
-            GoalProofResponse.of(
-                GoalProof(
-                    id = 0L,
-                    user = User(
-                        id = 0L,
-                        oauthTokenPayload = "sample",
-                        fcmTokenPayload = "sample",
-                        nickname = Nickname("sample"),
-                        deletedAt = null,
-                        createdAt = null,
-                        updatedAt = null
-                    ),
-                    goal = Goal(
-                        id = 1L,
-                        type = BettingType.BILLING,
-                        content = Content("sample"),
-                        threshold = Threshold(0),
-                        startDate = LocalDateTime.now().plusDays(1),
-                        endDate = LocalDateTime.now().plusMonths(1),
-                        createdAt = LocalDateTime.now(),
-                        updatedAt = LocalDateTime.now(),
-                        deletedAt = null
-                    ),
-                    document = Document("sample"),
-                    deletedAt = null,
-                    createdAt = null,
-                    updatedAt = null
-                )
+            GoalProofRetrieveResponse(
+                id = 0L,
+                userId = 0L,
+                goalId = 0L,
+                document = Document("Fake Document")
             )
         )
     }
 
     fun retrieve(): GoalProofRetrieveResponse {
         return GoalProofRetrieveResponse(
-            user = UserResponse.of(
-                User(
-                    id = 0L,
-                    oauthTokenPayload = "sample",
-                    fcmTokenPayload = "sample",
-                    nickname = Nickname("sample"),
-                    deletedAt = null,
-                    createdAt = null,
-                    updatedAt = null
-                )
-            ),
-            goal = Goal(
-                id = 1L,
-                type = BettingType.BILLING,
-                content = Content("sample"),
-                threshold = Threshold(0),
-                startDate = LocalDateTime.now().plusDays(1),
-                endDate = LocalDateTime.now().plusMonths(1),
-                createdAt = LocalDateTime.now(),
-                updatedAt = LocalDateTime.now(),
-                deletedAt = null
-            ),
-            document = Document("sample")
+            id = 0L,
+            userId = 0L,
+            goalId = 0L,
+            document = Document("Fake Document")
         )
     }
 }
--- raisedragon-api/src/main/kotlin/com/whatever/raisedragon/applicationservice/UserApplicationService.kt
+++ raisedragon-api/src/main/kotlin/com/whatever/raisedragon/applicationservice/UserApplicationService.kt
@@ -1,10 +1,8 @@
 package com.whatever.raisedragon.applicationservice
 
-import com.whatever.raisedragon.applicationservice.dto.UserResponse
 import com.whatever.raisedragon.controller.user.UserCreateUpdateResponse
 import com.whatever.raisedragon.controller.user.UserRetrieveResponse
 import com.whatever.raisedragon.domain.user.Nickname
-import com.whatever.raisedragon.domain.user.User
 import com.whatever.raisedragon.domain.user.UserService
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -16,33 +14,17 @@ class UserApplicationService(
 ) {
     fun create(): UserCreateUpdateResponse {
         return UserCreateUpdateResponse(
-            UserResponse.of(
-                User(
-                    0L,
-                    "sample",
-                    "sample",
-                    Nickname("sample"),
-                    null,
-                    null,
-                    null
-                )
+            UserRetrieveResponse(
+                userId = 0L,
+                nickname = Nickname("sample")
             )
         )
     }
 
     fun retrieve(): UserRetrieveResponse {
-        val sampleUser = User(
-            0L,
-            "sample",
-            "sample",
-            Nickname("sample"),
-            null,
-            null,
-            null
-        )
         return UserRetrieveResponse(
-            sampleUser.id,
-            sampleUser.nickname
+            userId = 0L,
+            nickname = Nickname("sample")
         )
     }
 }
