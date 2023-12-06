84ae05a3ca69d80d163d94698bea78138a2ab309
Diger <kitten.diger@gmail.com>
2023-12-06 14:49:28 +0900

    Add EntityDto Extension Method [toDto()]

--- raisedragon-core/src/main/kotlin/com/whatever/raisedragon/domain/goalproof/GoalProof.kt
+++ raisedragon-core/src/main/kotlin/com/whatever/raisedragon/domain/goalproof/GoalProof.kt
@@ -1,16 +1,25 @@
 package com.whatever.raisedragon.domain.goalproof
 
 import com.whatever.raisedragon.domain.goal.Goal
-import com.whatever.raisedragon.domain.goal.GoalEntity
 import com.whatever.raisedragon.domain.user.User
 import java.time.LocalDateTime
 
 data class GoalProof(
     val id: Long,
-    val user: User,
-    val goal: Goal,
+    val userId: Long,
+    val goalId: Long,
     val document: Document,
     var deletedAt: LocalDateTime?,
     var createdAt: LocalDateTime?,
     var updatedAt: LocalDateTime?
+)
+
+fun GoalProofEntity.toDto(): GoalProof = GoalProof(
+    id = id,
+    userId = userEntity.id,
+    goalId = goalEntity.id,
+    document = document,
+    deletedAt = deletedAt,
+    createdAt = createdAt,
+    updatedAt = updatedAt
 )
--- raisedragon-core/src/main/kotlin/com/whatever/raisedragon/domain/refreshtoken/RefreshToken.kt
+++ raisedragon-core/src/main/kotlin/com/whatever/raisedragon/domain/refreshtoken/RefreshToken.kt
@@ -1,12 +1,21 @@
 package com.whatever.raisedragon.domain.refreshtoken
 
-import com.whatever.raisedragon.domain.user.User
 import java.time.LocalDateTime
 
 data class RefreshToken(
+    val id: Long,
     val payload: String?,
-    val user: User,
+    val userId: Long,
     var deletedAt: LocalDateTime?,
     var createdAt: LocalDateTime?,
     var updatedAt: LocalDateTime?
+)
+
+fun RefreshTokenEntity.toDto(): RefreshToken = RefreshToken(
+    id = id,
+    payload = payload,
+    userId = userEntity.id,
+    deletedAt = deletedAt,
+    createdAt = createdAt,
+    updatedAt = updatedAt
 )
