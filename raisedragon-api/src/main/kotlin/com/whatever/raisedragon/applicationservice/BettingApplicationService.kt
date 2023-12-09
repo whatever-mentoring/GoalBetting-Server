794641cba6104d1df729be2fe049c90f491e9ea1
Diger <kitten.diger@gmail.com>
2023-12-10 01:16:53 +0900

    - Refactor Validate Duplicated Goal Creation - Add `UserEntity` Column Goal Entity

--- raisedragon-api/src/main/kotlin/com/whatever/raisedragon/applicationservice/BettingApplicationService.kt
+++ raisedragon-api/src/main/kotlin/com/whatever/raisedragon/applicationservice/BettingApplicationService.kt
@@ -1,12 +1,11 @@
 package com.whatever.raisedragon.applicationservice
 
+import com.whatever.raisedragon.common.exception.BaseException
+import com.whatever.raisedragon.common.exception.ExceptionCode
 import com.whatever.raisedragon.controller.betting.BettingCreateUpdateResponse
 import com.whatever.raisedragon.controller.betting.BettingRetrieveResponse
 import com.whatever.raisedragon.domain.betting.BettingService
 import com.whatever.raisedragon.domain.betting.PredictionType
-import com.whatever.raisedragon.domain.goal.GoalService
-import com.whatever.raisedragon.domain.user.UserService
-import com.whatever.raisedragon.security.authentication.UserInfo
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -14,8 +13,6 @@ import org.springframework.transaction.annotation.Transactional
 @Transactional
 class BettingApplicationService(
     private val bettingService: BettingService,
-    private val userService: UserService,
-    private val goalService: GoalService
 ) {
 
     fun create(
@@ -23,20 +20,26 @@ class BettingApplicationService(
         goalId: Long,
         predictionType: PredictionType
     ): BettingCreateUpdateResponse {
-
-        val betting = bettingService.create(
-            user = userService.loadById(userId),
-            goal = goalService.loadById(goalId),
-            predictionType = predictionType
-        )
-
-        return BettingCreateUpdateResponse(
-            BettingRetrieveResponse(
-                id = betting.id,
-                userId = betting.userId,
-                goalId = betting.goalId,
+        if (canBet(userId, goalId)) {
+            val betting = bettingService.create(
+                userId = userId,
+                goalId = goalId,
                 predictionType = predictionType
             )
+
+            return BettingCreateUpdateResponse(
+                BettingRetrieveResponse(
+                    id = betting.id,
+                    userId = betting.userId,
+                    goalId = betting.goalId,
+                    predictionType = predictionType
+                )
+            )
+        }
+
+        throw BaseException.of(
+            exceptionCode = ExceptionCode.E409_CONFLICT,
+            executionMessage = "이미 배팅에 참여한 다짐입니다."
         )
     }
 
@@ -48,4 +51,9 @@ class BettingApplicationService(
             predictionType = PredictionType.SUCCESS,
         )
     }
+
+    private fun canBet(
+        userId: Long,
+        goalId: Long
+    ) = bettingService.loadUserAndGoal(userId, goalId) == null
 }
--- raisedragon-core/src/main/kotlin/com/whatever/raisedragon/domain/betting/BettingRepository.kt
+++ raisedragon-core/src/main/kotlin/com/whatever/raisedragon/domain/betting/BettingRepository.kt
@@ -1,7 +1,15 @@
 package com.whatever.raisedragon.domain.betting
 
+import com.whatever.raisedragon.domain.goal.GoalEntity
+import com.whatever.raisedragon.domain.user.UserEntity
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface BettingRepository : JpaRepository<BettingEntity, Long>
+interface BettingRepository : JpaRepository<BettingEntity, Long> {
+
+    fun findByUserEntityAndGoalEntity(
+        userEntity: UserEntity,
+        goalEntity: GoalEntity
+    ): BettingEntity?
+}
