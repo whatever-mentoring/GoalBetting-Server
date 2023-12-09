5614071c35d816e0f2a66c864593d36d2e93352e
Diger <kitten.diger@gmail.com>
2023-12-10 01:16:53 +0900

    - Refactor Validate Duplicated Goal Creation - Add `UserEntity` Column Goal Entity

--- raisedragon-core/src/main/kotlin/com/whatever/raisedragon/domain/betting/BettingService.kt
+++ raisedragon-core/src/main/kotlin/com/whatever/raisedragon/domain/betting/BettingService.kt
@@ -1,29 +1,40 @@
 package com.whatever.raisedragon.domain.betting
 
-import com.whatever.raisedragon.domain.goal.Goal
-import com.whatever.raisedragon.domain.goal.fromDto
-import com.whatever.raisedragon.domain.user.User
-import com.whatever.raisedragon.domain.user.fromDto
+import com.whatever.raisedragon.domain.goal.GoalRepository
+import com.whatever.raisedragon.domain.user.UserRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 @Transactional
 class BettingService(
-    private val bettingRepository: BettingRepository
+    private val bettingRepository: BettingRepository,
+    private val userRepository: UserRepository,
+    private val goalRepository: GoalRepository
 ) {
     fun create(
-        user: User,
-        goal: Goal,
+        userId: Long,
+        goalId: Long,
         predictionType: PredictionType
     ): Betting {
         val betting = bettingRepository.save(
             BettingEntity(
-                userEntity = user.fromDto(),
-                goalEntity = goal.fromDto(),
+                userEntity = userRepository.findById(userId).get(),
+                goalEntity = goalRepository.findById(goalId).get(),
                 predictionType = predictionType
             )
         )
         return betting.toDto()
+    }
+
+    @Transactional(readOnly = true)
+    fun loadUserAndGoal(
+        userId: Long,
+        goalId: Long
+    ): BettingEntity? {
+        return bettingRepository.findByUserEntityAndGoalEntity(
+            userEntity = userRepository.findById(userId).get(),
+            goalEntity = goalRepository.findById(goalId).get()
+        )
     }
 }
