77cd9b9c19604f479ac76185a1802512d5b9f5f2
Jaemin.Park <once29@naver.com>
2023-12-10 02:32:27 +0900

    Configure spring-security

---
+++ raisedragon-api/src/main/kotlin/com/whatever/raisedragon/config/SecurityConfig.kt
@@ -0,0 +1,74 @@
+package com.whatever.raisedragon.config
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.whatever.raisedragon.security.handler.HttpStatusAccessDeniedHandler
+import com.whatever.raisedragon.security.handler.HttpStatusAuthenticationEntryPoint
+import com.whatever.raisedragon.security.jwt.JwtAgent
+import com.whatever.raisedragon.security.jwt.JwtFilter
+import org.springframework.context.annotation.Bean
+import org.springframework.http.HttpHeaders
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+
+@EnableWebSecurity
+class SecurityConfig(private val jwtAgent: JwtAgent, private val objectMapper: ObjectMapper) {
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        return http
+            .cors { it.configurationSource(corsConfigurationSource()) }
+            .csrf { it.disable() }
+            .httpBasic { it.disable() }
+            .formLogin { it.disable() }
+            .authorizeHttpRequests {
+                it.anyRequest().authenticated() // TODO: 추후 인가 추가
+            }
+            .addFilterBefore(JwtFilter(jwtAgent, objectMapper), UsernamePasswordAuthenticationFilter::class.java)
+            .exceptionHandling {
+                it.authenticationEntryPoint(HttpStatusAuthenticationEntryPoint())
+                it.accessDeniedHandler(HttpStatusAccessDeniedHandler())
+            }
+            .build()
+    }
+
+    @Bean
+    fun webSecurityCustomizer(): WebSecurityCustomizer {
+        return WebSecurityCustomizer {
+            it.ignoring().requestMatchers(
+                // TODO: Remove after providing test accessToken api
+                "/**",
+                // swagger
+                "/api-docs/**", "/swagger-ui/**", "/swagger-resources/**", "/v3/api-docs/**",
+                // error page
+                "/error/**",
+                // Auth endpoints
+                "/v1/auth/**"
+            )
+        }
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration().apply {
+            addAllowedMethod("*")
+            addAllowedHeader("*")
+            exposedHeaders = listOf(HttpHeaders.AUTHORIZATION, HttpHeaders.CONTENT_TYPE)
+            maxAge = 3600L
+        }
+
+        return UrlBasedCorsConfigurationSource().apply {
+            registerCorsConfiguration("/api/**", configuration)
+        }
+    }
+
+    companion object {
+        const val DEFAULT_ROLE_NAME = "USER"
+    }
+
+}
---
+++ raisedragon-api/src/main/kotlin/com/whatever/raisedragon/security/handler/HttpStatusAccessDeniedHandler.kt
@@ -0,0 +1,21 @@
+package com.whatever.raisedragon.security.handler
+
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.security.web.access.AccessDeniedHandler
+
+class HttpStatusAccessDeniedHandler : AccessDeniedHandler {
+    private val log: Logger = LoggerFactory.getLogger(HttpStatusAccessDeniedHandler::class.java)
+
+    override fun handle(
+        request: HttpServletRequest?,
+        response: HttpServletResponse,
+        accessDeniedException: org.springframework.security.access.AccessDeniedException
+    ) {
+        log.debug("Access Denied: ${accessDeniedException.message}")
+        response.status = HttpStatus.FORBIDDEN.value()
+    }
+}
