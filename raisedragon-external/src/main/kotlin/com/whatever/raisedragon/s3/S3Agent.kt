9b7cd380f646859b8aec4ec8583f763a1d014899
Jaemin.Park <once29@naver.com>
2023-12-10 03:52:01 +0900

    Fix public interface of S3Agent

--- raisedragon-external/src/main/kotlin/com/whatever/raisedragon/s3/S3Agent.kt
+++ raisedragon-external/src/main/kotlin/com/whatever/raisedragon/s3/S3Agent.kt
@@ -5,8 +5,9 @@ import org.springframework.web.multipart.MultipartFile
 interface S3Agent {
 
     /**
+     * @param directory S3 버킷에 파일을 저장할 Directory 이름입니다.
      * @param multipartFile 사용자가 전송한 정적파일입니다.
-     * 해당 파일을 S3에 업로드 한 이후, 해당 파일에 접근할 수 있는 PresignedURL을 반환해주세요.
+     * @return 해당 파일을 S3에 업로드 한 이후, 해당 파일에 접근할 수 있는 url을 반환해합니다.
      */
-    fun upload(multipartFile: MultipartFile): PresignedURL
+    fun upload(directory: String, file: MultipartFile): String
 }
