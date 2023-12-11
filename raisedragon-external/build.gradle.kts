9e551c70c685ecd177db6987fff58115ffbfcd0d
Jaemin.Park <once29@naver.com>
2023-12-11 19:45:10 +0900

    Implement S3Agent

--- raisedragon-external/build.gradle.kts
+++ raisedragon-external/build.gradle.kts
@@ -3,4 +3,7 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-validation:3.0.4")
     implementation("org.springframework.boot:spring-boot-starter-aop:3.0.4")
+
+    // aws
+    implementation("org.springframework.cloud:spring-cloud-starter-aws:2.2.6.RELEASE")
 }
---
+++ raisedragon-external/src/main/kotlin/com/whatever/raisedragon/aws/s3/S3AgentImpl.kt
@@ -0,0 +1,41 @@
+package com.whatever.raisedragon.aws.s3
+
+import com.amazonaws.AmazonServiceException
+import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.s3.model.ObjectMetadata
+import org.springframework.stereotype.Component
+import org.springframework.web.multipart.MultipartFile
+import java.io.IOException
+import java.util.*
+
+@Component
+class S3AgentImpl(
+    private val amazonS3: AmazonS3,
+    private val s3Properties: S3Config.S3Properties
+) : S3Agent {
+    override fun upload(directory: String, file: MultipartFile): String {
+        val fileName = String.format("%s%s-%s", directory, UUID.randomUUID(), file.originalFilename)
+        val bucket = s3Properties.bucketName
+        runCatching {
+            val objectMetadata = getObjectMetadataFromFile(file)
+            val inputStream = file.inputStream
+            amazonS3.putObject(bucket, fileName, inputStream, objectMetadata)
+        }.onFailure { exception ->
+            when (exception) {
+                is IOException -> throw IllegalStateException("S3 File I/O Error", exception)
+                is AmazonServiceException -> throw IllegalStateException(
+                    message = "Failed to upload the file ($fileName)",
+                    cause = exception
+                )
+            }
+        }
+        return "${s3Properties.domain}/$fileName"
+    }
+
+    private fun getObjectMetadataFromFile(imageFile: MultipartFile): ObjectMetadata {
+        return ObjectMetadata().apply {
+            contentType = imageFile.contentType
+            contentLength = imageFile.size
+        }
+    }
+}
