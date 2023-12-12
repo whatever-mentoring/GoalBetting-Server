60d15f69d7caf9db427096c2c8fa51791eab9b29
Diger <kitten.diger@gmail.com>
2023-12-13 01:34:03 +0900

    - Add Modify Goal

--- raisedragon-api/src/main/kotlin/com/whatever/raisedragon/applicationservice/GoalApplicationService.kt
+++ raisedragon-api/src/main/kotlin/com/whatever/raisedragon/applicationservice/GoalApplicationService.kt
@@ -3,10 +3,9 @@ package com.whatever.raisedragon.applicationservice
 import com.whatever.raisedragon.common.exception.BaseException
 import com.whatever.raisedragon.common.exception.ExceptionCode
 import com.whatever.raisedragon.controller.goal.GoalResponse
-import com.whatever.raisedragon.domain.goal.BettingType
-import com.whatever.raisedragon.domain.goal.Content
-import com.whatever.raisedragon.domain.goal.GoalService
-import com.whatever.raisedragon.domain.goal.Threshold
+import com.whatever.raisedragon.domain.goal.*
+import com.whatever.raisedragon.domain.user.UserService
+import com.whatever.raisedragon.domain.user.fromDto
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.time.LocalDateTime
@@ -15,6 +14,7 @@ import java.time.LocalDateTime
 @Service
 class GoalApplicationService(
     private val goalService: GoalService,
+    private val userService: UserService
 ) {
 
     @Transactional
@@ -78,6 +78,49 @@ class GoalApplicationService(
         return response
     }
 
+    @Transactional
+    fun modifyGoal(
+        userId: Long,
+        goalId: Long,
+        content: String
+    ): GoalResponse {
+        val goal = goalService.loadById(goalId)
+        isNotUsersGoal(goal, userId)
+        isAlreadyStarted(goal)
+
+        val modifiedGoal = goalService.modify(
+            goal = goal,
+            userEntity = userService.loadById(userId).fromDto(),
+            content = content
+        )
+
+        return GoalResponse(
+            id = modifiedGoal.id,
+            type = modifiedGoal.type,
+            content = modifiedGoal.content,
+            threshold = modifiedGoal.threshold,
+            startDate = modifiedGoal.startDate,
+            endDate = modifiedGoal.endDate
+        )
+    }
+
+    private fun isNotUsersGoal(goal: Goal, userId: Long) {
+        if (goal.userId != userId) {
+            throw BaseException.of(
+                exceptionCode = ExceptionCode.E400_BAD_REQUEST,
+                executionMessage = "다짐을 수정하는 중, 수정 권한이 없는 리소스에 대한 요청은 수행할 수 없습니다."
+            )
+        }
+    }
+
+    private fun isAlreadyStarted(goal: Goal) {
+        if (goal.startDate < LocalDateTime.now()) {
+            throw BaseException.of(
+                exceptionCode = ExceptionCode.E400_BAD_REQUEST,
+                executionMessage = "다짐을 수정하는 중, 이미 시작된 다짐은 수정할 수 없습니다."
+            )
+        }
+    }
 
     private fun isNumberOfGoalUnderOneHundred(userId: Long): Boolean {
         return goalService.loadAllByUserId(userId).size < 100
