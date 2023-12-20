da57aca8dbab921f015511d0d1e1113a5bf7ce87
Jaemin.Park <once29@naver.com>
2023-12-21 01:58:09 +0900

    Fix to emit Authorization AOP from S3Controller

--- raisedragon-api/src/main/kotlin/com/whatever/raisedragon/controller/external/S3Controller.kt
+++ raisedragon-api/src/main/kotlin/com/whatever/raisedragon/controller/external/S3Controller.kt
@@ -2,9 +2,8 @@ package com.whatever.raisedragon.controller.external
 
 import com.whatever.raisedragon.applicationservice.S3ApplicationService
 import com.whatever.raisedragon.common.Response
-import com.whatever.raisedragon.common.aop.Auth
-import com.whatever.raisedragon.common.aop.AuthContext
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.PostMapping
@@ -16,11 +15,11 @@ import org.springframework.web.multipart.MultipartFile
 @Tag(name = "S3", description = "S3 API")
 @RestController
 @RequestMapping("/v1/s3")
+@SecurityRequirement(name = "Authorization")
 class S3Controller(
     private val s3ApplicationService: S3ApplicationService
 ) {
 
-    @Auth
     @Operation(summary = "Upload Gifticon", description = "기프티콘을 업로드합니다.")
     @PostMapping(
         consumes = [MediaType.MULTIPART_FORM_DATA_VALUE],
@@ -29,9 +28,6 @@ class S3Controller(
     fun create(
         @RequestPart multipartFile: MultipartFile
     ): Response<String> {
-        AuthContext.getUser()
-        return Response.success(
-            s3ApplicationService.upload(multipartFile)
-        )
+        return Response.success(s3ApplicationService.upload(multipartFile))
     }
 }
