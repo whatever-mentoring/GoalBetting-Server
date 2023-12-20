6074101bdd7a2fc0d3732e49f6baf3389dcf4420
Jaemin.Park <once29@naver.com>
2023-12-21 00:56:44 +0900

    Remove duplicated CORS config logic

--- raisedragon-api/src/main/kotlin/com/whatever/raisedragon/config/WebConfigurer.kt
+++ raisedragon-api/src/main/kotlin/com/whatever/raisedragon/config/WebConfigurer.kt
@@ -3,7 +3,6 @@ package com.whatever.raisedragon.config
 import com.whatever.raisedragon.security.resolver.UserInfoArgumentResolver
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
-import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
@@ -14,13 +13,4 @@ class WebConfigurer : WebMvcConfigurer {
     override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
         resolvers.add(UserInfoArgumentResolver())
     }
-
-//    override fun addCorsMappings(registry: CorsRegistry) {
-//        registry.addMapping("/**")
-//            .allowedOrigins("/**")
-//            .allowedMethods("GET", "POST", "PUT", "DELETE")
-//            .allowedHeaders("Origin", "Content-Type", "Accept", "Authorization")
-//            .allowCredentials(true)
-//            .maxAge(3600)
-//    }
 }
