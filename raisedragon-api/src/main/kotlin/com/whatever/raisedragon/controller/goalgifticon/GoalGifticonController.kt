f11f3bd99bb86b71346674a0843299c5b9dc7e91
Jaemin.Park <once29@naver.com>
2023-12-21 01:58:26 +0900

    Fix to emit Authorization AOP from GoalGifticonController

--- raisedragon-api/src/main/kotlin/com/whatever/raisedragon/controller/goalgifticon/GoalGifticonController.kt
+++ raisedragon-api/src/main/kotlin/com/whatever/raisedragon/controller/goalgifticon/GoalGifticonController.kt
@@ -2,9 +2,10 @@ package com.whatever.raisedragon.controller.goalgifticon
 
 import com.whatever.raisedragon.applicationservice.GoalGifticonApplicationService
 import com.whatever.raisedragon.common.Response
-import com.whatever.raisedragon.common.aop.Auth
-import com.whatever.raisedragon.common.aop.AuthContext
+import com.whatever.raisedragon.security.authentication.UserInfo
+import com.whatever.raisedragon.security.resolver.GetAuth
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
@@ -17,49 +18,56 @@ class GoalGifticonController(
     private val goalGifticonApplicationService: GoalGifticonApplicationService
 ) {
 
-    @Auth
     @ResponseStatus(HttpStatus.CREATED)
-    @Operation(summary = "다짐 내 기프티콘 생성 API", description = "기프티콘 생성")
+    @Operation(
+        summary = "다짐 내 기프티콘 생성 API",
+        description = "기프티콘 생성",
+        security = [SecurityRequirement(name = "Authorization")]
+    )
     @PostMapping
     fun create(
-        // @GetAuth userInfo: UserInfo,
+        @GetAuth userInfo: UserInfo,
         @Valid request: GoalGifticonCreateRequest,
     ): Response<GoalGifticonResponse> {
-        AuthContext.getUser()
         return Response.success(
             goalGifticonApplicationService.createAndUploadGifticon(
-                userId = AuthContext.getUser().id!!,
+                userId = userInfo.id,
                 goalId = request.goalId,
                 uploadedURL = request.gifticonURL
             )
         )
     }
 
-    @Auth
     @ResponseStatus(HttpStatus.CREATED)
-    @Operation(summary = "다짐 내 기프티콘 조회 API", description = "다짐 내 기프티콘 조회")
+    @Operation(
+        summary = "다짐 내 기프티콘 조회 API",
+        description = "다짐 내 기프티콘 조회",
+        security = [SecurityRequirement(name = "Authorization")]
+    )
     @GetMapping("/{goalId}")
-    fun retrieve(@PathVariable goalId: Long): Response<GoalGifticonResponse> {
-        AuthContext.getUser()
+    fun retrieve(@GetAuth userInfo: UserInfo, @PathVariable goalId: Long): Response<GoalGifticonResponse> {
         return Response.success(
             goalGifticonApplicationService.retrieveByGoalId(
                 goalId = goalId,
-                userId = AuthContext.getUser().id!!,
+                userId = userInfo.id,
             )
         )
     }
 
-    @Auth
     @ResponseStatus(HttpStatus.CREATED)
-    @Operation(summary = "다짐 내 기프티콘 수정 API", description = "다짐 내 기프티콘 수정")
+    @Operation(
+        summary = "다짐 내 기프티콘 수정 API",
+        description = "다짐 내 기프티콘 수정",
+        security = [SecurityRequirement(name = "Authorization")]
+    )
     @PutMapping
     fun update(
+        @GetAuth userInfo: UserInfo,
         @RequestBody goalGifticonRequest: GoalGifticonRequest
     ): Response<GoalGifticonResponse> {
-        AuthContext.getUser()
         return Response.success(
             goalGifticonApplicationService.updateGifticonURLByGoalId(
-                userId = AuthContext.getUser().id!!,
+                userId = userInfo.id,
                 goalId = goalGifticonRequest.goalId,
                 gifticonURL = goalGifticonRequest.gifticonURL,
             )
