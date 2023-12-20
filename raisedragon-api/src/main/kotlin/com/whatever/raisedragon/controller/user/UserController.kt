33cbf7c11b7dbd9eed78cdf1c84c1ac5def7ffd4
Jaemin.Park <once29@naver.com>
2023-12-21 01:43:49 +0900

    Fix to emit Authorization AOP from UserController

--- raisedragon-api/src/main/kotlin/com/whatever/raisedragon/controller/user/UserController.kt
+++ raisedragon-api/src/main/kotlin/com/whatever/raisedragon/controller/user/UserController.kt
@@ -2,24 +2,26 @@ package com.whatever.raisedragon.controller.user
 
 import com.whatever.raisedragon.applicationservice.UserApplicationService
 import com.whatever.raisedragon.common.Response
-import com.whatever.raisedragon.common.aop.Auth
 import com.whatever.raisedragon.common.aop.AuthContext
+import com.whatever.raisedragon.security.authentication.UserInfo
+import com.whatever.raisedragon.security.resolver.GetAuth
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.*
 
 @Tag(name = "User", description = "User API")
 @RestController
 @RequestMapping("/v1/user")
+@SecurityRequirement(name = "Authorization")
 class UserController(
     private val userApplicationService: UserApplicationService
 ) {
 
-    @Auth
     @Operation(summary = "Request User retrieve API", description = "요청한 사용자를 조회합니다.")
     @GetMapping
     fun retrieveMe(
-        // @GetAuth userInfo: UserInfo
+        @GetAuth userInfo: UserInfo
     ): Response<UserRetrieveResponse> {
         return Response.success(
             userApplicationService.retrieve(
@@ -28,11 +30,10 @@ class UserController(
         )
     }
 
-    @Auth
     @Operation(summary = "User retrieve API", description = "특정 사용자를 조회합니다.")
     @GetMapping("{userId}")
     fun retrieveTarget(
-        // @GetAuth userInfo: UserInfo,
+        @GetAuth userInfo: UserInfo,
         @PathVariable userId: Long
     ): Response<UserRetrieveResponse> {
         return Response.success(
@@ -42,31 +43,28 @@ class UserController(
         )
     }
 
-    @Auth
-    @Operation(summary = "User Nickname update API", description = "닉네임을 수정합니다.")
+    @Operation(
+        summary = "User Nickname update API",
+        description = "닉네임을 수정합니다."
+    )
     @PutMapping
     fun updateNickname(
-        // @GetAuth userInfo: UserInfo,
+        @GetAuth userInfo: UserInfo,
         @RequestBody userNicknameUpdateRequest: UserNicknameUpdateRequest
     ): Response<UserRetrieveResponse> {
         return Response.success(
             userApplicationService.updateNickname(
-                id = AuthContext.getUser().id!!,
+                id = userInfo.id,
                 nickname = userNicknameUpdateRequest.nickname
             )
         )
     }
 
-    @Auth
     @Operation(summary = "User delete API", description = "회원탈퇴")
     @DeleteMapping
     fun delete(
-        // @GetAuth userInfo: UserInfo,
+        @GetAuth userInfo: UserInfo,
     ): Response<Unit> {
-        return Response.success(
-            userApplicationService.delete(
-                AuthContext.getUser().id!!
-            )
-        )
+        return Response.success(userApplicationService.delete(userInfo.id))
     }
 }
