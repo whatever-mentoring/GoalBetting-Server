4b091f15e0a200a9e1a66ee3b6abc20688c7d9c7
Jaemin.Park <once29@naver.com>
2023-12-21 01:58:17 +0900

    Fix to emit Authorization AOP from GoalController

--- raisedragon-api/src/main/kotlin/com/whatever/raisedragon/controller/goal/GoalController.kt
+++ raisedragon-api/src/main/kotlin/com/whatever/raisedragon/controller/goal/GoalController.kt
@@ -2,10 +2,11 @@ package com.whatever.raisedragon.controller.goal
 
 import com.whatever.raisedragon.applicationservice.GoalApplicationService
 import com.whatever.raisedragon.common.Response
-import com.whatever.raisedragon.common.aop.Auth
-import com.whatever.raisedragon.common.aop.AuthContext
 import com.whatever.raisedragon.domain.goal.Content
+import com.whatever.raisedragon.security.authentication.UserInfo
+import com.whatever.raisedragon.security.resolver.GetAuth
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
@@ -18,95 +19,103 @@ class GoalController(
     private val goalApplicationService: GoalApplicationService
 ) {
 
-    @Auth
     @ResponseStatus(HttpStatus.CREATED)
-    @Operation(summary = "Creating Goal API", description = "다짐을 생성합니다.")
+    @Operation(
+        summary = "Creating Goal API",
+        description = "다짐을 생성합니다.",
+        security = [SecurityRequirement(name = "Authorization")]
+    )
     @PostMapping
     fun create(
         @Valid @RequestBody request: GoalCreateRequest,
-        // @GetAuth userinfo: UserInfo
+        @GetAuth userinfo: UserInfo
     ): Response<GoalResponse> {
-        AuthContext.getUser()
         return Response.success(
             goalApplicationService.createGoal(
                 bettingType = request.type,
                 content = Content(request.content),
                 startDate = request.startDate,
                 endDate = request.endDate,
-                userId = AuthContext.getUser().id!!
+                userId = userinfo.id
             )
         )
     }
 
-    @Auth
-    @Operation(summary = "Retrieving Goal API", description = "다짐 단일 조회")
+    @Operation(
+        summary = "Retrieving Goal API",
+        description = "다짐 단일 조회",
+        security = [SecurityRequirement(name = "Authorization")]
+    )
     @GetMapping("/{goalId}")
     fun retrieveOne(
-        @PathVariable goalId: Long,
-        // @GetAuth userInfo: UserInfo
+        @PathVariable goalId: Long
     ): Response<GoalResponse> {
-        AuthContext.getUser()
         return Response.success(goalApplicationService.retrieveGoal(goalId))
     }
 
-    @Auth
-    @Operation(summary = "Retrieving Multiple Goal API", description = "요청자의 모든 다짐 조회")
+    @Operation(
+        summary = "Retrieving Multiple Goal API",
+        description = "요청자의 모든 다짐 조회",
+        security = [SecurityRequirement(name = "Authorization")]
+    )
     @GetMapping
     fun retrieveMany(
-        // @GetAuth userInfo: UserInfo
+        @GetAuth userInfo: UserInfo
     ): Response<List<GoalResponse>> {
-        AuthContext.getUser()
-        return Response.success(
-            goalApplicationService.retrieveAllByUserId(
-                AuthContext.getUser().id!!
-            )
-        )
+        return Response.success(goalApplicationService.retrieveAllByUserId(userInfo.id))
     }
 
-    @Auth
-    @Operation(summary = "Retrieving GoalBetting API", description = "해당 다짐에 대한 모든 배팅 조회")
+    @Operation(
+        summary = "Retrieving GoalBetting API",
+        description = "해당 다짐에 대한 모든 배팅 조회",
+        security = [SecurityRequirement(name = "Authorization")]
+    )
     @GetMapping("/betting/{goalId}")
     fun retrieveParticipant(
-        @PathVariable goalId: Long
+        @PathVariable goalId: Long,
+        @GetAuth userInfo: UserInfo
     ): Response<GoalRetrieveParticipantResponse> {
-        AuthContext.getUser()
         return Response.success(
             goalApplicationService.retrieveGoalBettingParticipant(
-                userId = AuthContext.getUser().id!!,
+                userId = userInfo.id,
                 goalId = goalId
             )
         )
     }
 
-    @Auth
-    @Operation(summary = "Modify Goal API", description = "다짐 세부 내용 수정")
+    @Operation(
+        summary = "Modify Goal API",
+        description = "다짐 세부 내용 수정",
+        security = [SecurityRequirement(name = "Authorization")]
+    )
     @PutMapping("{goalId}")
     fun modifyGoal(
-        // @GetAuth userInfo: UserInfo,
+        @GetAuth userInfo: UserInfo,
         @PathVariable goalId: Long,
         @RequestBody goalModifyRequest: GoalModifyRequest
     ): Response<GoalResponse> {
-        AuthContext.getUser()
         return Response.success(
             goalApplicationService.modifyGoal(
-                userId = AuthContext.getUser().id!!,
+                userId = userInfo.id,
                 goalId = goalId,
                 content = goalModifyRequest.content
             )
         )
     }
 
-    @Auth
-    @Operation(summary = "Delete Goal API", description = "다짐 삭제")
+    @Operation(
+        summary = "Delete Goal API",
+        description = "다짐 삭제",
+        security = [SecurityRequirement(name = "Authorization")]
+    )
     @DeleteMapping
     fun deleteGoal(
-        // @GetAuth userInfo: UserInfo,
+        @GetAuth userInfo: UserInfo,
         @RequestBody goalDeleteRequest: GoalDeleteRequest
     ): Response<Unit> {
-        AuthContext.getUser()
         return Response.success(
             goalApplicationService.deleteGoal(
-                userId = AuthContext.getUser().id!!,
+                userId = userInfo.id,
                 goalId = goalDeleteRequest.goalId,
             )
         )
