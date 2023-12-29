78ee01d244ab3db97bf41ce25abff9073feacee3
Diger <kitten.diger@gmail.com>
2023-12-29 17:10:43 +0900

    Add Broken BadWordFilter AOP

--- raisedragon-api/src/main/kotlin/com/whatever/raisedragon/common/aop/badwordfilter/BadWordFilterAspect.kt
+++ raisedragon-api/src/main/kotlin/com/whatever/raisedragon/common/aop/badwordfilter/BadWordFilterAspect.kt
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component
 
 @Aspect
 @Component
-class AuthAspect {
+class BadWordFilterAspect {
 
     @Around("@annotation($BASE_PACKAGE)")
     fun filter(pjp: ProceedingJoinPoint): Any {
