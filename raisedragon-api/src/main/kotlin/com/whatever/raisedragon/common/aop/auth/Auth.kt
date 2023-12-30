a7823c6675f7b2c76b0b32981561b5296d26447e
Diger <kitten.diger@gmail.com>
2023-12-30 17:00:03 +0900

    Add AOP Priority

--- raisedragon-api/src/main/kotlin/com/whatever/raisedragon/common/aop/auth/Auth.kt
+++ raisedragon-api/src/main/kotlin/com/whatever/raisedragon/common/aop/auth/Auth.kt
@@ -1,5 +1,8 @@
 package com.whatever.raisedragon.common.aop.auth
 
+import org.springframework.core.annotation.Order
+
 @Target(AnnotationTarget.FUNCTION)
 @Retention(AnnotationRetention.RUNTIME)
+@Order(value = 2)
 annotation class Auth
--- raisedragon-api/src/main/kotlin/com/whatever/raisedragon/common/aop/badwordfilter/ValidateBadWord.kt
+++ raisedragon-api/src/main/kotlin/com/whatever/raisedragon/common/aop/badwordfilter/ValidateBadWord.kt
@@ -2,11 +2,13 @@ package com.whatever.raisedragon.common.aop.badwordfilter
 
 import jakarta.validation.Constraint
 import jakarta.validation.Payload
+import org.springframework.core.annotation.Order
 import kotlin.reflect.KClass
 
 @Constraint(validatedBy = [BadWordValidator::class])
 @Target(AnnotationTarget.FIELD)
 @Retention(AnnotationRetention.RUNTIME)
+@Order(value = 1)
 annotation class ValidateBadWord(
     val message: String = "비속어가 포함되어 있습니다.",
     val groups: Array<KClass<out Any>> = [],
