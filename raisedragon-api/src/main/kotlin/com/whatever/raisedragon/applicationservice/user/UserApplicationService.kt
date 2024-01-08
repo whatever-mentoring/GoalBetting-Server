3985d78f418db99831aa4cccc591b00bc3d6b51c
Jaemin.Park <once29@naver.com>
2024-01-08 17:07:56 +0900

    Write test codes for UserRepository

--- raisedragon-api/src/main/kotlin/com/whatever/raisedragon/applicationservice/user/UserApplicationService.kt
+++ raisedragon-api/src/main/kotlin/com/whatever/raisedragon/applicationservice/user/UserApplicationService.kt
@@ -1,9 +1,11 @@
-package com.whatever.raisedragon.applicationservice
+package com.whatever.raisedragon.applicationservice.user
 
+import com.whatever.raisedragon.applicationservice.user.dto.UserNicknameDuplicatedResponse
+import com.whatever.raisedragon.applicationservice.user.dto.UserNicknameDuplicatedServiceRequest
+import com.whatever.raisedragon.applicationservice.user.dto.UserNicknameUpdateServiceRequest
+import com.whatever.raisedragon.applicationservice.user.dto.UserRetrieveResponse
 import com.whatever.raisedragon.common.exception.BaseException
 import com.whatever.raisedragon.common.exception.ExceptionCode
-import com.whatever.raisedragon.controller.user.UserNicknameDuplicatedResponse
-import com.whatever.raisedragon.controller.user.UserRetrieveResponse
 import com.whatever.raisedragon.domain.betting.BettingService
 import com.whatever.raisedragon.domain.gifticon.GifticonService
 import com.whatever.raisedragon.domain.goal.GoalService
@@ -34,8 +36,8 @@ class UserApplicationService(
     }
 
     @Transactional
-    fun updateNickname(id: Long, nickname: String): UserRetrieveResponse {
-        val user = userService.updateNickname(id, nickname)
+    fun updateNickname(request: UserNicknameUpdateServiceRequest): UserRetrieveResponse {
+        val user = userService.updateNickname(request.userId, request.nickname)
         return UserRetrieveResponse(
             userId = user.id!!,
             nickname = user.nickname,
@@ -67,10 +69,10 @@ class UserApplicationService(
     }
 
     fun isNicknameDuplicated(
-        nickname: String
+        request: UserNicknameDuplicatedServiceRequest
     ): UserNicknameDuplicatedResponse {
         return UserNicknameDuplicatedResponse(
-            nicknameIsDuplicated = userService.isNicknameDuplicated(nickname)
+            nicknameIsDuplicated = userService.isNicknameDuplicated(request.nickname)
         )
     }
 }
---
+++ raisedragon-core/src/test/kotlin/com/whatever/raisedragon/domain/user/UserRepositoryTest.kt
@@ -0,0 +1,46 @@
+package com.whatever.raisedragon.domain.user
+
+import com.whatever.raisedragon.RepositoryTestSupport
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+
+class UserRepositoryTest : RepositoryTestSupport() {
+
+    @Autowired
+    private lateinit var userRepository: UserRepository
+
+    @DisplayName("특정 OAuthTokenPayload를 갖는 UserEntity를 조회한다.")
+    @Test
+    fun findByOauthTokenPayload() {
+        // given
+        val oAuthTokenPayload = "testTokenPayload"
+        val userEntity1 = UserEntity(nickname = Nickname("User1"), oauthTokenPayload = oAuthTokenPayload)
+        val userEntity2 = UserEntity(nickname = Nickname("User2"), oauthTokenPayload = "sampleTokenPlayoad")
+        userRepository.saveAll(listOf(userEntity1, userEntity2))
+
+        // when
+        val foundUserEntity = userRepository.findByOauthTokenPayload(oAuthTokenPayload)
+
+        // then
+        assertThat(foundUserEntity).isEqualTo(userEntity1)
+    }
+
+    @DisplayName("특정 닉네임을 갖고 있는 UserEntity가 있는지 조회한다.")
+    @Test
+    fun existsByNickname() {
+        // given
+        val nickname = Nickname("User1")
+        val userEntity1 = UserEntity(nickname = Nickname("User1"))
+        val userEntity2 = UserEntity(nickname = Nickname("User2"))
+        userRepository.saveAll(listOf(userEntity1, userEntity2))
+
+        // when
+        val isExist = userRepository.existsByNickname(nickname)
+
+        // then
+        assertThat(isExist).isTrue()
+    }
+
+}
