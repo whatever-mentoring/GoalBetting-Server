45f735052c432bc9af6b258c350ab1fe48ba8f0a
Jaemin.Park <once29@naver.com>
2024-03-26 20:21:42 +0900

    Implement ApplicationServiceTestSupport

---
+++ raisedragon-api/src/test/kotlin/com/whatever/raisedragon/applicationservice/betting/ApplicationServiceTestSupport.kt
@@ -0,0 +1,8 @@
+package com.whatever.raisedragon.applicationservice.betting
+
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+
+@ActiveProfiles("test")
+@SpringBootTest
+interface ApplicationServiceTestSupport
